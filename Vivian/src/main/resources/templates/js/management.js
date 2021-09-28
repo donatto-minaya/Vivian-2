0312c75aa495d35c7182d57a1d855cbcc7068e29
donatto-minaya <ottanod22@gmail.com>
2021-09-27 22:38:10 -0500

    Nuevos archivos para la página

---
+++ Vivian/src/main/resources/templates/js/management.js
@@ -0,0 +1,12 @@
+//prevent the user see the code
+/*onkeydown = e => {
+	let tecla = e.which || e.keyCode;
+	
+	if ( e.ctrlKey ) {
+		if ( tecla === 85 || tecla === 83 || tecla === 16 || tecla === 73 || tecla === 63 || tecla === 67 || tecla === 123) {
+			e.preventDefault();
+			e.stopPropagation();	
+		}
+	}
+} */
+
